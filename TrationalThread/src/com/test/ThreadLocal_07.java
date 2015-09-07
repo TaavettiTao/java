e5672013da03fd9dc1c19141829eef34f437a299
TaavettiTao <1634093385@qq.com>
2015-09-07 22:42:59 +0800

    add

---
+++ TrationalThread/src/com/test/ThreadLocal_07.java
@@ -0,0 +1,51 @@
+package com.test;
+
+import java.util.Random;
+
+/**
+ * 
+ * @author taoweiwei
+ * ThreadLocal：实现线程范围内单个变量共享，一个ThreadLocal对象代表一个变量，故其中只能放一个数据。
+ * ThreadLocal并不是一个Thread，而是Thread的局部变量,其本质也是通过内部定义的Map实现线程范围内的共享变量的。
+ */
+public class ThreadLocal_07 {
+	
+	    //定义静态变量ThreadLocalMap，key为当前线程，value为当前线程所拥有的变量
+	    private  static  ThreadLocal<Integer> threadLocal=new ThreadLocal<Integer>();
+	
+		public static void main(String[] args) {
+			
+			for(int i=0;i<2;i++){//启动两个线程
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						int data=new Random().nextInt();//放入数据
+						
+						System.out.println(Thread.currentThread().getName()+" has putted data:"+data);
+						
+						//保存当前线程所拥有的信息
+						threadLocal.set(data);
+						
+						new A().get();
+						new B().get();
+					}
+				}).start();
+			}
+			
+		}
+		
+		static class A{
+			
+			public void get(){
+				System.out.println("A from "+Thread.currentThread().getName()+" get data:"+threadLocal.get());
+			}
+			
+			
+		}
+		
+		static class B{
+			public void get(){
+				System.out.println("B from "+Thread.currentThread().getName()+" get data:"+threadLocal.get());
+			}
+		}
+}
--- TrationalThread/src/com/test/ThreadShareData5.java
+++ TrationalThread/src/com/test/ThreadShareData5.java
@@ -7,6 +7,17 @@ import java.util.Random;
  * @author taoweiwei
  * 线程范围内共享变量：example one
  * 在该类中由于定义了静态变量data,所以多个线程共享静态变量data的数值,所以多个线程拿到的值是一样的
+ * 线程并发执行是不可控的，无法知道那个线程先执行，那个线程后执行，都是通过CPU调度的，
+ * 如果一段代码会被多个线程操作，则那么出现的情况是无法预料的；
+ * 即使该程序代码是顺序的，但是可能线程1执行到A区域代码时，线程2已经执行到了A区域之后的B区域代码。
+ * 所以下面程序出现以下情况是很正常的：
+ * Thread-0 has putted data:-1649628587
+   Thread-1 has putted data:-1146026723
+   A from Thread-1 get data:-1146026723
+   A from Thread-0 get data:-1146026723
+   B from Thread-1 get data:-1146026723
+   B from Thread-0 get data:-1146026723
+   
  */
 public class ThreadShareData5 {
 	
